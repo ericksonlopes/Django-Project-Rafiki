f12c71847c05b07d259da18f305e450c5faf3ffb
Erickson-Lopes-dev <ofc.erickson@gmail.com>
2020-07-29 17:19:08 -0300

    parte de login finalizada

---
+++ Rafiki/urls.py
@@ -0,0 +1,9 @@
+
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('accounts/', include('django.contrib.auth.urls')),
+    path(' ', include('website.urls'), name='home'),
+]
